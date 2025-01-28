f555b66b353ec92b2cdacbbc4bc43ff851e32d5b
Anna Paliy <annpaliy2015@gmail.com>
2025-01-28 19:05:39 +0300

    status code 300 added

---
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -0,0 +1,25 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanTest {
+    @Test
+    void shouldReturnPostTest() {
+
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(300)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
